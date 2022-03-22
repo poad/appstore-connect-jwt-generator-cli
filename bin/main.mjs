5adec3f49bb5c7a71df5169397c885835481ce2d
Kenji Saito <ken-yo@mbr.nifty.com>
2022-03-22 18:33:51 +0900

    Bumps node modules

--- bin/main.mjs
+++ bin/main.mjs
@@ -4,7 +4,7 @@ import i from "arg";
 import d from "chalk-template";
 import e from "log4js";
 import j from "path";
-e.configure(JSON.parse("{\"appenders\":{\"out\":{\"layout\":{\"pattern\":\"%m%n\",\"type\":\"pattern\"},\"type\":\"stdout\"}},\"categories\":{\"default\":{\"appenders\":[\"out\"],\"level\":\"info\"}}}"));
+e.configure(JSON.parse('{"appenders":{"out":{"layout":{"pattern":"%m%n","type":"pattern"},"type":"stdout"}},"categories":{"default":{"appenders":["out"],"level":"info"}}}'));
 const b = e.getLogger(), f = {
     "--help": {
         type: Boolean,
@@ -39,7 +39,7 @@ try {
     const a = i({
         ...k,
         ...l
-    }), g = JSON.parse(Buffer.from(c.readFileSync(j.resolve("package.json"), JSON.parse("{\"flag\":\"r\"}"))).toString()), m = d`
+    }), g = JSON.parse(Buffer.from(c.readFileSync(j.resolve("package.json"), JSON.parse('{"flag":"r"}'))).toString()), m = d`
   {bold USAGE}
 
       {dim $} {bold ${Object.keys(g.bin).pop()}} [--help] --string {underline some-arg}
@@ -52,7 +52,7 @@ try {
       --issuerId {underline issuer-id}   Issuer ID for AppStore Connect API
 `;
     void 0 !== a["--help"] && (b.error(m), process.exit(0)), void 0 !== a["--version"] && (b.info(g.version), process.exit(0)), void 0 === a["--cert"] && (b.error("The certificate file path must be specified."), process.exit(1)), void 0 === a["--keyId"] && (b.error("Key ID must be specified."), process.exit(1)), void 0 === a["--issuerId"] && (b.error("Issuer ID must be specified."), process.exit(1));
-    const n = a["--cert"], o = c.readFileSync(n, JSON.parse("{\"flag\":\"r\"}")), p = h.tokenSync(o, a["--issuerId"], a["--keyId"], void 0);
+    const n = a["--cert"], o = c.readFileSync(n, JSON.parse('{"flag":"r"}')), p = h.tokenSync(o, a["--issuerId"], a["--keyId"], void 0);
     b.info(d`
 {bold token}
 ${p}
