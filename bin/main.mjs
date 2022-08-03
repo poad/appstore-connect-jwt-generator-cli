89cc16d104d1a470011ed016f46fbba994e55881
Kenji Saito <ken-yo@mbr.nifty.com>
2022-08-03 23:59:46 +0900

    Bumps node modules

--- bin/main.mjs
+++ bin/main.mjs
@@ -1,11 +1,11 @@
-import a from 'appstore-connect-jwt-generator-core';
-import * as b from 'fs';
-import c from 'arg';
-import d from 'chalk-template';
-import e from 'log4js';
-import f from 'path';
-e.configure(JSON.parse('{"appenders":{"out":{"layout":{"pattern":"%m%n","type":"pattern"},"type":"stdout"}},"categories":{"default":{"appenders":["out"],"level":"info"}}}'));
-let g = e.getLogger(), h = {
+import e from 'appstore-connect-jwt-generator-core';
+import * as r from 'fs';
+import i from 'arg';
+import o from 'chalk-template';
+import t from 'log4js';
+import s from 'path';
+t.configure(JSON.parse('{"appenders":{"out":{"layout":{"pattern":"%m%n","type":"pattern"},"type":"stdout"}},"categories":{"default":{"appenders":["out"],"level":"info"}}}'));
+let n = t.getLogger(), p = {
     '--help': {
         type: Boolean,
         alias: '-h'
@@ -26,21 +26,21 @@ let g = e.getLogger(), h = {
         type: String,
         alias: '-i'
     }
-}, i = Object.keys(h).map((a)=>{
-    let b = JSON.parse("{}");
-    return b[a] = h[a].type, b;
-}).reduce((a, b)=>Object.assign(b, a)), j = Object.keys(h).map((a)=>{
-    let b = JSON.parse("{}");
-    return b[h[a].alias] = a, b;
-}).reduce((a, b)=>Object.assign(b, a)), k = {
-    ...i,
-    ...j
+}, a = Object.keys(p).map((e)=>{
+    let r = JSON.parse("{}");
+    return r[e] = p[e].type, r;
+}).reduce((e, r)=>Object.assign(r, e)), l = Object.keys(p).map((e)=>{
+    let r = JSON.parse("{}");
+    return r[p[e].alias] = e, r;
+}).reduce((e, r)=>Object.assign(r, e)), d = {
+    ...a,
+    ...l
 };
 try {
-    let l = c(k), m = JSON.parse(Buffer.from(b.readFileSync(f.resolve('package.json'), JSON.parse('{"flag":"r"}'))).toString()), n = d`
+    let m = i(d), f = JSON.parse(Buffer.from(r.readFileSync(s.resolve('package.json'), JSON.parse('{"flag":"r"}'))).toString()), y = o`
   {bold USAGE}
 
-      {dim $} {bold ${Object.keys(m.bin).pop()}} [--help] --string {underline some-arg}
+      {dim $} {bold ${Object.keys(f.bin).pop()}} [--help] --string {underline some-arg}
 
   {bold OPTIONS}
       --help                 Shows this help message
@@ -49,12 +49,12 @@ try {
       --keyId {underline key-id}         Key ID for AppStore Connect API
       --issuerId {underline issuer-id}   Issuer ID for AppStore Connect API
 `;
-    void 0 !== l['--help'] && (g.error(n), process.exit(0)), void 0 !== l['--version'] && (g.info(m.version), process.exit(0)), void 0 === l['--cert'] && (g.error('The certificate file path must be specified.'), process.exit(1)), void 0 === l['--keyId'] && (g.error('Key ID must be specified.'), process.exit(1)), void 0 === l['--issuerId'] && (g.error('Issuer ID must be specified.'), process.exit(1));
-    let o = l['--cert'], p = b.readFileSync(o, JSON.parse('{"flag":"r"}')), q = a.tokenSync(p, l['--issuerId'], l['--keyId'], void 0);
-    g.info(d`
+    void 0 !== m['--help'] && (n.error(y), process.exit(0)), void 0 !== m['--version'] && (n.info(f.version), process.exit(0)), void 0 === m['--cert'] && (n.error('The certificate file path must be specified.'), process.exit(1)), void 0 === m['--keyId'] && (n.error('Key ID must be specified.'), process.exit(1)), void 0 === m['--issuerId'] && (n.error('Issuer ID must be specified.'), process.exit(1));
+    let c = m['--cert'], u = r.readFileSync(c, JSON.parse('{"flag":"r"}')), v = e.tokenSync(u, m['--issuerId'], m['--keyId'], void 0);
+    n.info(o`
 {bold token}
-${q}
+${v}
 `);
-} catch (r) {
-    g.error(r), process.exit(1);
+} catch (I) {
+    n.error(I), process.exit(1);
 }
