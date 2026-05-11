52c73c7aecc7298732c79426175c02bbac25ac06
Kenji Saito <ken-yo@mbr.nifty.com>
2026-05-11 23:00:37 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -2,42 +2,36 @@
 
 CUR=$(pwd)
 
-CURRENT=$(cd $(dirname $0);pwd)
+CURRENT=$(cd "$(dirname "$0")" || exit;pwd)
 echo "${CURRENT}"
 
-cd "${CURRENT}"
+cd "${CURRENT}" || exit
 git pull --prune
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
 
-result=$?
-if [ $result -ne 0 ]; then
-  cd "${CUR}"
-  exit $result
-fi
 echo ""
 pwd
-pnpm self-update && pnpm install && pnpm up -r && pnpm audit --fix override && pnpm up -r && rm -rf bin && pnpm build && pnpm package && rm -rf node_modules && pnpm install --production && git add bin node_modules -f
-result=$?
-if [ $result -ne 0 ]; then
-  cd "${CUR}"
-  exit $result
+
+if ! (pnpm self-update && pnpm install && pnpm up -r && pnpm audit --fix override && pnpm up -r && rm -rf bin && pnpm build && pnpm package && rm -rf node_modules && pnpm install --production && git add bin node_modules -f); then
+  cd "${CUR}" || exit
+  exit 1
 fi
 
-cd "${CURRENT}"
+cd "${CURRENT}" || exit
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
 git commit -am "Bumps node modules" && git push
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
 
-cd "${CUR}"
+cd "${CUR}" || exit
